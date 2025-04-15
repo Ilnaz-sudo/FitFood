586237c829f6efc0a38be47da8abd0b8cd1f8698
Ilnaz-sudo <ilnaz.akhmetvaleev@mail.ru>
2025-04-15 19:33:20 +0300

    Комменты случайно удалились, добавил гланую страницу, вкладку с камерой и вкладку с профилем

--- main.py
+++ main.py
@@ -1,54 +1,78 @@
 from kivy.lang import Builder
+from kivy.uix.label import Label
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivymd.app import MDApp
+from kivy.uix.recycleview.views import RecycleDataViewBehavior
+from kivy.properties import BooleanProperty
 
-# Подгружаем kv файлы
 Builder.load_file("gender.kv")
 Builder.load_file("age.kv")
+Builder.load_file("menu.kv")
+Builder.load_file("camera.kv")
+Builder.load_file("profile.kv")
 
 
-# Окно выбора пола
 class Gender(Screen):
-    # Функция для реализации задач при первом обращении к окну
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.ids.progress_bar.value = 25  # от 100 процентов/устанавливаем значение прогрессбара
+        self.ids.progress_bar.value = 25  # от 100 процентов
 
-    # Меняем цвета кнопки, активируем кнопку Next при нажатии на кнопку
     def male_button_press(self):
-        self.ids.male.md_bg_color = 0, 0, 0, 1  # Черный цвет фона кнопки
-        self.ids.male.text_color = 1, 1, 1, 1  # Белый цвет текста
+        self.ids.male.md_bg_color = 0, 0, 0, 1
+        self.ids.male.text_color = 1, 1, 1, 1
 
-        self.ids.next.disabled = False  # Включаем кнопку Next
+        self.ids.next.disabled = False
 
-        self.ids.female.md_bg_color = 0.9, 0.9, 0.9, 1  # Светло серый цвет фона кнопки
-        self.ids.female.text_color = 0, 0, 0, 1  # Белый цвет текста
+        self.ids.female.md_bg_color = 0.9, 0.9, 0.9, 1
+        self.ids.female.text_color = 0, 0, 0, 1
 
-    # Меняем цвета кнопки, активируем кнопку Next при нажатии на кнопку
     def female_button_press(self):
-        self.ids.female.md_bg_color = 0, 0, 0, 1  # Черный цвет фона кнопки
-        self.ids.female.text_color = 1, 1, 1, 1  # Белый цвет текста
+        self.ids.female.md_bg_color = 0, 0, 0, 1
+        self.ids.female.text_color = 1, 1, 1, 1
 
-        self.ids.next.disabled = False  # Включаем кнопку Next
+        self.ids.next.disabled = False
 
-        self.ids.male.md_bg_color = 0.9, 0.9, 0.9, 1  # Светло серый цвет фона кнопки
-        self.ids.male.text_color = 0, 0, 0, 1   # Белый цвет текста
+        self.ids.male.md_bg_color = 0.9, 0.9, 0.9, 1
+        self.ids.male.text_color = 0, 0, 0, 1
 
-#Окно выбора возраста
+class SelectableLabel(RecycleDataViewBehavior, Label):
+    selected = BooleanProperty(False)
+
 class Age(Screen):
-    # Функция для реализации задач при первом обращении к окну
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.ids.progress_bar.value = 40  # от 100 процентов/устанавливаем значение прогрессбара
+        self.ids.progress_bar.value = 40  # от 100 процентв
 
-#Основной класс
+
+
+
+
+
+class Menu(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+class Camera(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+class Profile(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
 class FitApp(MDApp):
     def build(self):
-        sm = ScreenManager()    # Позволяет нам переключаться между окнами
-        sm.add_widget(Gender(name='gender'))    # Открываем окно gender
-        sm.add_widget(Age(name='age'))  # Открываем окно age
-        return sm   # Возращаем наши окна
+        sm = ScreenManager()
+        sm.add_widget(Gender(name='gender'))
+        sm.add_widget(Age(name='age'))
+        sm.add_widget(Menu(name='menu'))
+        sm.add_widget(Camera(name='camera'))
+        sm.add_widget(Profile(name='profile'))
+        return sm
 
-# Основной цикл программы
+
 if __name__ == "__main__":
-    FitApp().run()  # Запускаем приложение
+    FitApp().run()
